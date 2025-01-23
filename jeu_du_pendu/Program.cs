369fafbe631f282363dddce3b2ec91361dae130c
Yangf <Yangf@HMONG>
2025-01-23 17:07:13 +0100

    Ajoutez des fichiers projet.

---
+++ jeu_du_pendu/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jeu_du_pendu
+{
+    internal class Program
+    {
+        static void Afficher(string mot, List<char> lettres)
+        {
+            int nb_index = mot.Count();
+            Console.WriteLine(nb_index);
+            
+            for (int i = 0; i < nb_index; i++)
+            {
+                char lettre = mot[i];
+                if (lettres.Contains(lettre))
+                {
+                    Console.Write(lettre+" ");
+                }
+                else
+                {
+                    Console.Write("_ ");
+                }
+            }
+            Console.WriteLine();
+            
+        }
+
+        static bool ToutesLettresDevinees(string mot, List<char> lettres)
+        {
+            List<string> deleteMot = new List<string>();
+            //Console.WriteLine(lettres.Count());
+            //Console.WriteLine(mot.Length);
+            for (int i=0 ;i < lettres.Count(); i++)
+            {
+                mot = mot.Replace(lettres[i].ToString(), "");
+           
+                if (mot.Length == 0)
+                {
+                    return true;
+                }
+          
+            }
+            Console.Write(mot);
+            
+            return false;
+        }
+
+        static char DemanderUnelettre()
+        {
+            string lettre = "";
+            while (true) 
+            {
+                Console.WriteLine("Donne une lettre ");
+                lettre = Console.ReadLine();
+                if (lettre.Length >= 2 || (int.TryParse(lettre, out int resultat) && resultat >= 0))
+                {
+                    Console.WriteLine("ERREUR: donne que une lettre");
+                }
+                else
+                {
+                    lettre = lettre.ToUpper();
+                    return lettre[0];
+                }
+            }
+     
+           
+        }
+        static void DevinerMot(string er)
+        {
+            List<char> devinerLettre = new List<char>();
+            const int NB_VIES = 6;
+            int pointdeVie = NB_VIES;
+        
+            while (pointdeVie > 0)
+            {
+                Console.WriteLine(Ascii.PENDU[NB_VIES - pointdeVie]); 
+                Afficher(er, devinerLettre);
+                Console.WriteLine();
+                var lettre = DemanderUnelettre();
+                Console.Clear();
+                if(er.Contains(lettre))
+                {
+                    Console.WriteLine("Correcte");
+                    devinerLettre.Add(lettre);
+                    var isCorrect = ToutesLettresDevinees(er, devinerLettre);
+                    if (isCorrect)
+                    {
+                        Console.Write("Gagner !!! ");
+                        break;
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("cette lettre n'est pas dans le mot");
+                    pointdeVie--;
+                    Console.WriteLine("Point de vie restant "+pointdeVie);
+                    if (pointdeVie == 0)
+                    {
+                        Console.WriteLine("perdu dommage: " + er);
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+         
+            Console.WriteLine(er+ " :Etais le mot a trouvé");
+        }
+
+        static string[] ChargerLesMots(string nomFichier)
+        {
+            try
+            {
+                return File.ReadAllLines(nomFichier);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Erreur de lecture du fichier");
+            }
+           return null;
+        
+        }
+        static void Main(string[] args)
+        {
+            var mots = ChargerLesMots("mots.txt");
+            if ((mots == null) || (mots.Length == 0))
+            {
+                Console.WriteLine("La liste de mots est vide");
+            }
+            else
+            {
+                Random rand = new Random();
+
+                int text_index = rand.Next(0, mots.Length);
+                Console.WriteLine("Le fichier text contient " + mots.Length + " mots");
+                Console.WriteLine("le numero aléatoire " + text_index);
+                string mot = mots[text_index].Trim().ToUpper();
+                DevinerMot(mot);
+            }
+
+            Console.WriteLine("Voulez-vous rejouer o/n");
+            string reponse = Console.ReadLine();
+            if (reponse == null || reponse == "n")
+            {
+                Console.WriteLine("Fini");
+            }
+            else
+            {
+                Main(args);
+            }
+        }
+    }
+}
---
+++ jeu_du_pendu/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("jeu_du_pendu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("jeu_du_pendu")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("45c45495-507a-4385-87a6-d7ee9fcb0e2f")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
